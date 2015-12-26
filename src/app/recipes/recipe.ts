0e4dea5ed4e967746f5845780f01d5f65a00d514
Keith Stewart <kgstewart@gmail.com>
2015-12-25 19:28:10 -0600

    add recipe instructions

--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,4 +1,12 @@
+import {IRecipeInstructions} from './recipeInstructions/recipeInstructions';
+
 export interface IRecipe {
     id: number;
     title: string;
+    notes?: string;
+    sourceURL?: string;
+    instructions?: IRecipeInstructions[];
+    prepTime?: any;
+    cookTime?: any;
+    totalTime?: string
 }
---
+++ src/app/recipes/recipeInstructions/recipeInstructions.ts
@@ -0,0 +1,4 @@
+export interface IRecipeInstructions {
+    order: number;
+    instruction: string;
+}
