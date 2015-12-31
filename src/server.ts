8647a93f10d739a254a308c117a3ef00174cc35d
Keith Stewart <kgstewart@gmail.com>
2015-12-30 23:54:39 -0600

    create separate client and server folders

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 // serve static files from current directory
-app.use(express.static('./src'));
+app.use(express.static('./src/client'));
 app.use(express.static('./'));
 process.chdir('./');
 app.get('*', function(req, res) {
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/client/index.html')
 });
 
 var server = app.listen(3000);
