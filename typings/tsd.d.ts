0fc3b54e4c4c14fb771dda0e259569d266a28d73
Keith Stewart <kgstewart@gmail.com>
2016-01-01 15:39:13 -0600

    add node and express tsd files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
